098bf2377c4ba498141b8590235c339ba1ccca4d
okiverny <oleg.kivernyk@gmail.com>
2024-11-25 21:49:23 +0100

    BM25 lexical search implementation

--- Retrieval.py
+++ Retrieval.py
@@ -8,14 +8,14 @@ class MisconceptRetrieval:
     def __init__(self, search_strategy: RetrievalStrategy) -> None:
         self._search_strategy = search_strategy
 
-    def background_strategy(self) -> RetrievalStrategy:
+    def retrieval_strategy(self) -> RetrievalStrategy:
         """Just in case we need a reference to one of RetrievalStrategy objects"""
         return self._search_strategy
 
-    def set_background_strategy(self, search_strategy: RetrievalStrategy) -> None:
+    def set_retrieval_strategy(self, search_strategy: RetrievalStrategy) -> None:
         """Usually, the MisconceptRetrieval allows replacing a RetrievalStrategy object at runtime."""
         self._search_strategy = search_strategy
 
-    def find_misconceptions(self, data):
-
-        return 0
+    def find_misconceptions(self, texts, queries, top_k):
+        results, scores = self._search_strategy.search_misconceptions(texts, queries, top_k)
+        return results, scores
--- SearchMethods.py
+++ SearchMethods.py
@@ -1,24 +1,55 @@
 from abc import ABC, abstractmethod
 import numpy as np
 import pandas as pd
-import time, tqdm, warnings
+import time, warnings
+from tqdm import tqdm
+from rank_bm25 import BM25Okapi
 from transformers import AutoTokenizer
 from sentence_transformers import SentenceTransformer, util
 
-from rank_bm25 import BM25Okapi
-from sklearn.feature_extraction import _stop_words
-import string
+from helpers import bm25_tokenizer, preprocess_text
+
 
 # Strategy interface
 class RetrievalStrategy(ABC):
     """This is the interface that declares operations common to all signal extraction algorithms/methods."""
     @abstractmethod
-    def search_misconceptions(self, data: pd.DataFrame):
+    def search_misconceptions(self, texts, queries, top_k):
         pass
 
 # Concrete Strategies
 class LexicalSearch(RetrievalStrategy):
-    def search_misconceptions(self, data: pd.DataFrame):
-        return data
+    #### BM25 Search (lexical search)
+    def search_misconceptions(self, texts, queries, top_k):
+        num_candidates=100
+        print('texts', texts)
+
+        tokenized_texts = []
+        for passage in tqdm(texts):
+            tokenized_texts.append(bm25_tokenizer(passage))
+
+        bm25 = BM25Okapi(tokenized_texts)
+
+        scores = np.zeros((len(queries), top_k))
+        results = [[0 for _ in range(top_k)] for _ in range(len(queries))]
+        # Processing queries
+        for iquery, query in enumerate(queries):
+            bm25_scores = bm25.get_scores(bm25_tokenizer(query))
+            top_n = np.argpartition(bm25_scores, -num_candidates)[-num_candidates:]
+            bm25_hits = [{'corpus_id': idx, 'score': bm25_scores[idx]} for idx in top_n]
+            bm25_hits = sorted(bm25_hits, key=lambda x: x['score'], reverse=True)
+
+            print('------> Query:\n', bm25_tokenizer(query) )
+            print('------> Query:\n', query )
+            print(f"Top-{top_k} lexical search (BM25) hits of the following query:")
+            
+            for top_i, hit in enumerate(bm25_hits[0:top_k]):
+                print("\t{:.3f}\t{}".format(hit['score'], texts[hit['corpus_id']].replace("\n", " ")))
+                scores[iquery][top_i] = hit['score']
+                results[iquery][top_i] = texts[hit['corpus_id']]
+
+        
+        return results, scores
+
     
     
