812c9e7c32062fa8cf0f745711750982bd4fce5e
okiverny <oleg.kivernyk@gmail.com>
2024-11-28 14:26:50 +0100

    BM25 search refinement

--- SearchMethods.py
+++ SearchMethods.py
@@ -22,7 +22,9 @@ class LexicalSearch(RetrievalStrategy):
     #### BM25 Search (lexical search)
     def search_misconceptions(self, texts, queries, top_k):
         num_candidates=100
-        print('texts', texts)
+        #print('texts', texts)
+
+        confusing_words = ['recall']
 
         tokenized_texts = []
         for passage in tqdm(texts):
@@ -34,17 +36,18 @@ class LexicalSearch(RetrievalStrategy):
         results = [[0 for _ in range(top_k)] for _ in range(len(queries))]
         # Processing queries
         for iquery, query in enumerate(queries):
-            bm25_scores = bm25.get_scores(bm25_tokenizer(query))
+            query_tokenized = [tok for tok in bm25_tokenizer(query) if tok not in confusing_words]
+            bm25_scores = bm25.get_scores(query_tokenized)
             top_n = np.argpartition(bm25_scores, -num_candidates)[-num_candidates:]
             bm25_hits = [{'corpus_id': idx, 'score': bm25_scores[idx]} for idx in top_n]
             bm25_hits = sorted(bm25_hits, key=lambda x: x['score'], reverse=True)
 
-            print('------> Query:\n', bm25_tokenizer(query) )
-            print('------> Query:\n', query )
-            print(f"Top-{top_k} lexical search (BM25) hits of the following query:")
+            #print('------> Query:\n', query )
+            #print('------> Query Tokens:\n', query_tokenized )
+            #print(f"Top-{top_k} lexical search (BM25) hits of the following query:")
             
             for top_i, hit in enumerate(bm25_hits[0:top_k]):
-                print("\t{:.3f}\t{}".format(hit['score'], texts[hit['corpus_id']].replace("\n", " ")))
+                #print("\t{:.3f}\t{}".format(hit['score'], texts[hit['corpus_id']].replace("\n", " ")))
                 scores[iquery][top_i] = hit['score']
                 results[iquery][top_i] = texts[hit['corpus_id']]
 
